67c2f8c562945e7e0a12427976ebb390bf1d44ee
Olivier Le Moign <olemoign@parsys.com>
2016-02-08 10:06:48 +0100

    Update domain model utility.

--- asset_tracker/utilities/domain_model.py
+++ asset_tracker/utilities/domain_model.py
@@ -1,26 +1,36 @@
+# -*- coding: utf-8 -*-
 import datetime
 
-import inflection
+from inflection import underscore
 import sqlalchemy
 import sqlalchemy.orm
 import sqlalchemy.ext.declarative
+import sqlalchemy.ext.hybrid
 
 
-AsciiString = sqlalchemy.String
+and_ = sqlalchemy.and_
+asc = sqlalchemy.asc
 backref = sqlalchemy.orm.backref
 Boolean = sqlalchemy.Boolean
-Bytes = sqlalchemy.String
 Date = sqlalchemy.Date
 DateTime = sqlalchemy.DateTime
+desc = sqlalchemy.desc
 Enum = sqlalchemy.Enum
 Field = sqlalchemy.Column
+Float = sqlalchemy.Float
 ForeignKey = sqlalchemy.ForeignKey
+ForeignKeyConstraint = sqlalchemy.ForeignKeyConstraint
 func = sqlalchemy.func
+hybrid_property = sqlalchemy.ext.hybrid.hybrid_property
 Integer = sqlalchemy.Integer
+join = sqlalchemy.join
 or_ = sqlalchemy.or_
 relationship = sqlalchemy.orm.relationship
+select = sqlalchemy.select
 String = sqlalchemy.Unicode
 Table = sqlalchemy.Table
+Text = sqlalchemy.Text
+Time = sqlalchemy.Time
 
 __author__ = 'Jean-Christophe Bohin <jc.bohin@phiropsi.com>'
 
@@ -33,7 +43,7 @@ class _DeclarativeMeta(sqlalchemy.ext.declarative.DeclarativeMeta):
     def __new__(mcs, name, bases, d):
         tablename = d.get('__tablename__')
         if not tablename and d.get('__table__') is None:
-            d['__tablename__'] = inflection.underscore(name)
+            d['__tablename__'] = underscore(name)
 
         return sqlalchemy.ext.declarative.DeclarativeMeta.__new__(mcs, name, bases, d)
 
