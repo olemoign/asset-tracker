a5c97882027190123f10cb55a7334446dd2a9ef9
Olivier Le Moign <olemoign@parsys.com>
2016-07-07 17:30:42 +0200

    Fix alembic metadata location.

--- migrations/env.py
+++ migrations/env.py
@@ -14,7 +14,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from parsys_utilities.model import Model
+from asset_tracker.models.asset_tracker import Model
 target_metadata = Model.metadata
 
 # other values from the config, defined by the needs of env.py,
