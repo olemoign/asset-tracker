1ad0b134937fd146309ce0c510cc7316c4d660fc
Olivier Le Moign <olemoign@parsys.com>
2017-01-31 18:50:06 +0100

    Linting.

--- migrations/versions/c8984e73a5de_status_and_calibration_next_as_columns.py
+++ migrations/versions/c8984e73a5de_status_and_calibration_next_as_columns.py
@@ -6,14 +6,14 @@ Create Date: 2017-01-31 15:28:31.638303
 
 """
 
+from alembic import op
+import sqlalchemy as sa
+
 # revision identifiers, used by Alembic.
 revision = 'c8984e73a5de'
 down_revision = '0a5fb17fc85e'
 branch_labels = None
 depends_on = None
-
-from alembic import op
-import sqlalchemy as sa
 
 
 def upgrade():
@@ -22,12 +22,12 @@ def upgrade():
         batch_op.add_column(sa.Column('status_id', sa.Integer(), nullable=True))
         batch_op.create_foreign_key(batch_op.f('fk_asset_status_id_event_status'),
                                     'event_status', ['status_id'], ['id'])
-        
+
         batch_op.alter_column('type', new_column_name='asset_type', existing_type=sa.Unicode(), nullable=False)
 
     with op.batch_alter_table('event', schema=None) as batch_op:
         batch_op.alter_column('removed_date', new_column_name='removed_at')
-        
+
         batch_op.alter_column('asset_id', existing_type=sa.Integer(), nullable=False)
         batch_op.alter_column('date', existing_type=sa.Date(), nullable=False)
         batch_op.alter_column('status_id', existing_type=sa.Integer(), nullable=False)
