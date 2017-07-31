fa4a0823baac240177784a1452d51eecb8e55ffc
abonamy <abonamy@parsys.com>
2017-07-31 15:49:07 +0200

    fix: exception if event list is empty

--- asset_tracker/views_asset.py
+++ asset_tracker/views_asset.py
@@ -286,6 +286,13 @@ class AssetsEndPoint(object):
             self.add_event()
 
         if self.form.get('event-removed'):
+            nb_removed_event = len(self.form.get('event-removed'))
+            nb_active_event = self.asset._history.filter_by(removed=False).count()
+            if nb_removed_event >= nb_active_event:
+                return dict(error=_('Event list need at least one element.'),
+                            asset=self.asset,
+                            **self.get_base_form_data())
+
             self.remove_events()
 
         self.update_status_and_calibration_next()
