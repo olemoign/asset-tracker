a50f87f8d12f112a89dad18021b0cc9088a265c5
abonamy <abonamy@parsys.com>
2017-08-29 18:48:13 +0200

    fix: data collection harmonization

--- asset_tracker/views_asset.py
+++ asset_tracker/views_asset.py
@@ -115,8 +115,10 @@ class AssetsEndPoint(object):
 
         tenants = self.get_create_read_tenants()
 
+        softwares = self.get_latest_softwares_version()
+
         return {'calibration_frequencies': CALIBRATION_FREQUENCIES_YEARS, 'equipments_families': equipments_families,
-                'statuses': statuses, 'tenants': tenants}
+                'statuses': statuses, 'tenants': tenants, 'softwares': softwares}
 
     def read_form(self):
         """Format form content according to our needs.
@@ -307,7 +309,7 @@ class AssetsEndPoint(object):
                  renderer='assets-create_update.html')
     def update_get(self):
         """Get asset update form: we need the base form data + the asset data."""
-        return dict(asset=self.asset, softwares=self.get_latest_softwares(), **self.get_base_form_data())
+        return dict(asset=self.asset, **self.get_base_form_data())
 
     @view_config(route_name='assets-update', request_method='POST', permission='assets-update',
                  renderer='assets-create_update.html')
