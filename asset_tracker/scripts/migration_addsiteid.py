76e8946964e33624594f5aa917293de496120417
abonamy <abonamy@parsys.com>
2018-07-17 12:26:39 +0200

    comment: add missing descriptions on migration script

--- asset_tracker/scripts/migration_addsiteid.py
+++ asset_tracker/scripts/migration_addsiteid.py
@@ -1,5 +1,6 @@
-"""Data migration: add site_id
+"""Data migration: add site_id to site
 
+!This migration should be done before the migration of the Cloud!
 Fill new site column `site_id` with random_id
 
 """
@@ -14,9 +15,10 @@ from asset_tracker.models import Site
 
 
 # SQLITE = 'sqlite:////home/user/project_root/tmp/parsys_cloud.sqlite'
-# PG_SQL = 'postgresql://user:password@localhost/postgres'
-AT_DB_PATH = ''
+# PG_SQL = 'postgresql://user:password@localhost/db_name'
+AST_DB_PATH = ''
 
+# the expected database state
 ALEMBIC_VERSION = '70292af6fd9e'
 
 Base = declarative_base()
@@ -45,37 +47,52 @@ def get_db_session(db_path):
     return db_session
 
 
-def check_db_state(db_session, alembic_version):
-    """Verify that the database is in the expected state."""
+def check_db_state(asset_db_session, alembic_version):
+    """Verify that the database is in the expected state.
+
+    Args:
+        asset_db_session (sqlalchemy.orm.session.Session): to query Asset-Tracker database.
+        alembic_version (str): the expected database state.
+
+    """
     try:
-        db_session.query(TmpAlembicVersion).filter_by(version_num=alembic_version).one()
+        asset_db_session.query(TmpAlembicVersion).filter_by(version_num=alembic_version).one()
 
     except NoResultFound as e:
         err_msg = 'The database expected state ({}) is not fulfilled.'.format(alembic_version)
         raise Exception(err_msg) from e
 
 
-def get_sites(db_session):
-    """Get sites without site_id."""
-    sites = db_session.query(Site).filter(Site.site_id.is_(None))
-    return sites
+def get_sites(asset_db_session):
+    """Get sites to update.
+
+    Get only sites without site_id.
+
+    Args:
+        asset_db_session (sqlalchemy.orm.session.Session): to query Asset-Tracker database.
+
+    Returns:
+        sites (Query): site list.
+
+    """
+    return asset_db_session.query(Site).filter(Site.site_id.is_(None))
 
 
 def data_migration():
     """Main function: fill the site_id column of site table."""
     # database connection
-    at_db_session = get_db_session(AT_DB_PATH)
+    asset_db_session = get_db_session(AST_DB_PATH)
 
     # perform migration in a specific state
-    check_db_state(at_db_session, ALEMBIC_VERSION)
+    check_db_state(asset_db_session, ALEMBIC_VERSION)
 
     # add random id to site_id column
-    sites = get_sites(at_db_session)
+    sites = get_sites(asset_db_session)
 
     i = 0
     for site in sites:
         site.site_id = random_id()
-        at_db_session.commit()
+        asset_db_session.commit()
         i += 1
 
     print('updated sites: {}'.format(i))
