9b1e647c401925fec1d1a80d169d8ccb804f2544
Olivier Le Moign <olemoign@parsys.com>
2018-07-24 11:38:36 +0200

    Simplify migration.

--- alembic/versions/70292af6fd9e_add_site_id.py
+++ alembic/versions/70292af6fd9e_add_site_id.py
@@ -6,9 +6,11 @@ Create Date: 2018-07-16 12:05:18.423951
 
 """
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
+from parsys_utilities.random import random_id
 
+from asset_tracker.models import Site
 
 # revision identifiers, used by Alembic.
 revision = '70292af6fd9e'
@@ -18,9 +20,21 @@ depends_on = None
 
 
 def upgrade():
+    connection = op.get_bind()
+    session = sa.orm.session.Session(bind=connection)
+
     with op.batch_alter_table('site', schema=None) as batch_op:
         batch_op.add_column(sa.Column('site_id', sa.Unicode()))
         batch_op.create_unique_constraint(batch_op.f('uq_site_site_id'), ['site_id'])
+    session.commit()
+
+    for site in session.query(Site).all():
+        site.site_id = random_id()
+    session.commit()
+
+    with op.batch_alter_table('site', schema=None) as batch_op:
+        batch_op.alter_column('site_id', nullable=False)
+    session.commit()
 
 
 def downgrade():
