005d2c14fc68e5f8636750c7eed9727245932f1d
Olivier Le Moign <olemoign@parsys.com>
2019-02-15 15:50:49 +0100

    Flake8.

--- alembic/versions/c915540f60cb_add_hardware_version.py
+++ alembic/versions/c915540f60cb_add_hardware_version.py
@@ -6,14 +6,14 @@ Create Date: 2019-02-15 15:15:58.463903
 
 """
 
+import sqlalchemy as sa
+from alembic import op
+
 # revision identifiers, used by Alembic.
 revision = 'c915540f60cb'
 down_revision = '70292af6fd9e'
 branch_labels = None
 depends_on = None
-
-import sqlalchemy as sa
-from alembic import op
 
 
 def upgrade():
