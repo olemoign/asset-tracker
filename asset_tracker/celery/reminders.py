c8337fc5f36a6c05bff869518975cf9c4b05bdc9
Tsy-Jon Lau <tlau@parsys.com>
2019-07-10 19:16:12 +0200

    feat(celery): add consumables expiration task and notification

--- asset_tracker/celery/reminders.py
+++ asset_tracker/celery/reminders.py
@@ -5,6 +5,8 @@ import transaction
 from celery.utils.log import get_task_logger
 from parsys_utilities.celery_app import app
 from parsys_utilities.celery_tasks import get_session_factory
+from sqlalchemy import or_
+from sqlalchemy.orm import joinedload
 from sentry_sdk import capture_exception
 
 from asset_tracker import models
@@ -61,3 +63,54 @@ def next_calibration(months=3):
 
             for tenant_id, assets in groupby_tenant:
                 notifications_assets.next_calibration(pyramid_config, tenant_id, assets, calibration_date)
+
+
+def search_notify_consumables_expiration(db_session, expiration_date, delay_days):
+    """Get equipments with consumables that will expire and send a notification to involved users.
+
+    Args:
+        db_session (sqlalchemy.orm.session.Session): sqlalchemy db session.
+        expiration_date (str): a reminder is sent x months before equipment expiration
+        delay_days (int): days before expiration
+    """
+    equipments = db_session.query(models.Equipment) \
+        .join(models.Asset) \
+        .options(joinedload(models.Equipment.family)) \
+        .filter(or_(models.Equipment.expiration_date_1 == expiration_date,
+                    models.Equipment.expiration_date_2 == expiration_date)) \
+        .all()
+
+    if equipments:
+        pyramid_config = app.conf.pyramid_config
+
+        for equipment in equipments:
+            notifications_assets.consumables_expiration(pyramid_config, equipment, expiration_date, delay_days)
+
+
+@app.task()
+def consumables_expiration_reminder():
+    """Remind involved users about equipment consumables expiration."""
+
+    try:
+        # Validate all mandatory config is present.
+        [app.conf.pyramid_config['app:main'][config] for config in MANDATORY_CONFIG]
+    except AttributeError as error:
+        capture_exception(error)
+        logger.error(error)
+        return -1
+
+    # To avoid Jan (28,29,30,31) + 1 month = Feb 28, convert months in days.
+    expiration_delays_and_dates = [
+        (180, arrow.utcnow().shift(days=180).format('YYYY-MM-DD')),
+        (30, arrow.utcnow().shift(days=30).format('YYYY-MM-DD')),
+        (0, arrow.utcnow().format('YYYY-MM-DD')),
+    ]
+
+    # Set up db connection.
+    session_factory = get_session_factory()
+
+    with transaction.manager:
+        db_session = models.get_tm_session(session_factory, transaction.manager)
+
+        for delay_days, expiration_date in expiration_delays_and_dates:
+            search_notify_consumables_expiration(db_session, expiration_date, delay_days)
--- asset_tracker/notifications/assets.py
+++ asset_tracker/notifications/assets.py
@@ -52,3 +52,52 @@ def next_calibration(ini_configuration, tenant_id, assets, calibration_date):
 
     logging_info = ['notify calibration date', [asset.id for asset in assets]]
     logger.info(logging_info)
+
+
+def consumables_expiration(ini_configuration, equipment, expiration_date, delay_days):
+    """Notify users with notifications-consumables right that the consumables of an equipment are expiring.
+
+    Args:
+        ini_configuration (configparser.ConfigParser): asset tracker configuration.
+        equipment (asset_tracker.models.Equipment)).
+        expiration_date (str): consumable expiration date (YYYY-MM-DD).
+        delay_days (int): number of days before expiration.
+    """
+    tenant_id = equipment.asset.tenant_id
+
+    tenant_config = TenantConfigurator(settings=ini_configuration, defaults=DEFAULT_CONFIG)
+    app_name = tenant_config.get_for_tenant('asset_tracker.cloud_name', tenant_id)
+    subject = _('${app_name} - Equipment consumables expiration reminder', mapping={'app_name': app_name})
+    text = 'emails/consumables_expiration.txt'
+    html = 'emails/consumables_expiration.html'
+
+    asset_url = '{}/assets/{}'.format(
+        ini_configuration['app:main'].get('asset_tracker.server_url'),
+        equipment.asset.id,
+    )
+    model = equipment.family.model.split('-')[0]
+
+    template_data = {
+        'app_name': app_name,
+        'model': model,
+        'asset_id': equipment.asset.asset_id,
+        'asset_url': asset_url,
+        'expiration_date': expiration_date,
+        'delay_days': delay_days,
+    }
+
+    # Template generation
+    emails = emails_renderer_offline(TEMPLATES_PATH, TRANSLATIONS_PATH, subject, text, html, template_data)
+    messages = {'email': emails}
+
+    # Asynchronous POST
+    disable_notifications = asbool(ini_configuration['app:main'].get('asset_tracker.dev.disable_notifications', False))
+    if disable_notifications:
+        logger.debug('Notifications are disabled.')
+    else:
+        json = {'level': 'info', 'message': messages,
+                'rights': ['notifications-consumables'], 'tenant': tenant_id}
+        notify_offline(ini_configuration, json)
+
+    logger.info('notify equipment consumables expiration date {} - {}'.format(
+        equipment.asset.asset_id, equipment.family.model))
