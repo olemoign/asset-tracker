a9c45c42768e28c5821b9272ba7b7d683d7034c6
Olivier Le Moign <olemoign@parsys.com>
2020-02-12 17:15:20 +0100

    Fix prod conf.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 set -Eeuo pipefail
 
-mkdir -p /srv/log/ /srv/log/celery/ /srv/log/nginx/
-mkdir -p /srv/data/ /srv/data/blobstore/ /srv/data/postgres/ /srv/data/redis/
-cp -n /opt/docker-compose.yml /srv
-cp -n /opt/production.ini /srv
-alembic -c /srv/production.ini upgrade head
+celery=false
+if [ "$1" == "--celery" ]
+then
+  celery=true
+  set -- "${@:2}"
+fi
+
+if [ "$celery" == true ]
+then
+  mkdir -p /srv/log/ /srv/log/celery/
+  mkdir -p /srv/data/ /srv/data/redis/
+  cp -n /opt/production.ini /srv
+else
+  mkdir -p /srv/log/ /srv/log/nginx/
+  mkdir -p /srv/data/ /srv/data/blobstore/ /srv/data/postgres/ /srv/data/redis/
+  cp -n /opt/docker-compose.yml /srv
+  cp -n /opt/production.ini /srv
+  alembic -c /srv/production.ini upgrade head
+fi
 
 exec "$@"
