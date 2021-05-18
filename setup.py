1f8be2b18d4e91ea82526055506fbcc224167182
Olivier Le Moign <olemoign@parsys.com>
2021-05-18 16:18:17 +0200

    feat: pin dependencies

--- setup.py
+++ setup.py
@@ -9,46 +9,47 @@ with open(here / 'README.md') as f:
 with open(here / 'CHANGES.md') as f:
     CHANGES = f.read()
 
-parsys_utilities = 'https://tracker.parsys.com/api/v4/projects/9/packages/pypi/files/ea8255c0f40c5ebe73d8298d1b4446330b0be483ce0e434aaf45cdf892d4ff62/parsys_utilities-3.6.0-py3-none-any.whl'  # noqa: E501
+tracker = 'https://tracker.parsys.com/api/v4/projects/9/packages/pypi/files'
+parsys_utilities_3_6_0 = f'{tracker}/ea8255c0f40c5ebe73d8298d1b4446330b0be483ce0e434aaf45cdf892d4ff62/parsys_utilities-3.6.0-py3-none-any.whl'  # noqa: E501
 requires = [
-    'alembic',
-    'arrow',
-    'celery[redis]',
-    'filedepot',
-    'importlib-resources',
-    'jinja2',
-    f'parsys-utilities @ {parsys_utilities}',
-    'paste',
-    'plaster-pastedeploy',
-    'psycopg2-binary',
+    'alembic==1.6.2',
+    'arrow==1.10',
+    'celery[redis]==5.0.5',
+    'filedepot==0.8.0',
+    'importlib-resources==5.1.3',
+    'jinja2==3.0.0',
+    f'parsys-utilities @ {parsys_utilities_3_6_0}',
+    'paste==3.5.0',
+    'plaster-pastedeploy==0.7',
+    'psycopg2-binary==2.8.6',
     'pyramid==1.10.8',
-    'pyramid-assetviews',
-    'pyramid-jinja2',
-    'pyramid-session-redis',
-    'pyramid-tm',
-    'python-dateutil',
-    'sentry-sdk',
-    'setuptools',
-    'sqlalchemy',
-    'transaction',
-    'waitress',
-    'webob',
-    'zope.sqlalchemy',
+    'pyramid-assetviews==1.0a3',
+    'pyramid-jinja2==2.8',
+    'pyramid-session-redis==1.6.1',
+    'pyramid-tm==2.4',
+    'python-dateutil==2.8.1',
+    'sentry-sdk==1.1.0',
+    'setuptools==56.0.0',
+    'sqlalchemy==1.4.15',
+    'transaction==3.0.1',
+    'waitress==2.0.0',
+    'webob==1.8.7',
+    'zope.sqlalchemy==1.4',
 ]
 
 optional = {
     'dev': [
-        'babel',
-        'pybabel-json-md',
-        'pyramid-debugtoolbar',
+        'babel==2.9.1',
+        'pybabel-json-md==0.1.0',
+        'pyramid-debugtoolbar==4.9',
     ],
     'qa': [
-        'flake8',
+        'flake8==3.9.2',
     ],
     'tests': [
-        'pytest',
-        'pytest-cov',
-        'webtest',
+        'pytest==6.2.4',
+        'pytest-cov==2.12.0',
+        'webtest==2.0.35',
     ],
 }
 
@@ -61,11 +62,13 @@ setup(
     description='Asset Tracker',
     long_description=f'{README}\n\n{CHANGES}',
     classifiers=[
-        'Programming Language :: Python',
-        'Framework :: Pyramid',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Framework :: Pyramid',
         'License :: Other/Proprietary License',
     ],
     author='Parsys',
