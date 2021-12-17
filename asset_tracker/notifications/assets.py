b6bed4d937c5bc6407769b070da9a6a5c4628037
Olivier Le Moign <olemoign@parsys.com>
2021-12-17 01:22:44 +0100

    feat: make sure git sees the right change

--- asset_tracker/notifications/assets.py
+++ asset_tracker/notifications/assets.py
@@ -1,6 +1,38 @@
 from pyramid.i18n import TranslationString as _
 
 
+def assets_calibration(request, tenant_id, assets, calibration_date):
+    """Notify an asset owner that the asset needs to be calibrated.
+
+    Args:
+        request (pyramid.request.Request).
+        tenant_id (str).
+        assets (list[asset_tracker.models.Asset]).
+        calibration_date (date): precise calibration date (YYYY-MM-DD).
+    """
+
+    template_data = {
+        'assets': assets,
+        'calibration_date': calibration_date,
+        'cloud_name': request.registry.settings['asset_tracker.cloud_name'],
+    }
+
+    # Email.
+    subject = _('Device calibration reminder')
+    text = 'emails/assets_calibration.txt'
+    html = 'emails/assets_calibration.html'
+    emails = request.notifier.render_emails(subject, text, html, template_data)
+
+    # Asynchronous POST.
+    request.notifier.notify({
+        'message': {'email': emails},
+        'rights': ['notifications-calibration'],
+        'tenant': tenant_id,
+    })
+
+    request.logger_technical.info(['notify calibration date', [asset.id for asset in assets]])
+
+
 def consumables_expiration(request, tenant_id, equipment, expiration_date, delay_days):
     """Notify users with notifications-consumables right that the consumables of an equipment are expiring.
 
