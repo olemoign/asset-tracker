8b8a7e31ec515828cbbc6b32630b17b02891cab6
Olivier Le Moign <olemoign@parsys.com>
2021-12-17 02:46:32 +0100

    feat(notifications): consumables

--- asset_tracker/celery/reminders.py
+++ asset_tracker/celery/reminders.py
@@ -30,13 +30,16 @@ def assets_calibration(months=3):
     if not assets:
         return
 
+    # groupby will transform the list, so we need to check its length now.
+    assets_number = len(assets)
+
     # Group assets by tenant.
     groupby_tenant = itertools.groupby(assets, key=lambda asset: asset.tenant.tenant_id)
 
     for tenant_id, assets in groupby_tenant:
         notifications.assets.assets_calibration(request, tenant_id, assets, calibration_date)
 
-    return len(assets)
+    return assets_number
 
 
 @app.task()
@@ -51,20 +54,40 @@ def consumables_expiration():
     for delay_days in expiration_delays:
         expiration_date = arrow.utcnow().shift(days=delay_days).naive
 
-        equipments = request.db_session.query(models.Equipment) \
+        assets = request.db_session.query(models.Asset, models.Consumable) \
+            .join(models.Asset.tenant) \
+            .join(models.Asset.equipments) \
             .join(models.Equipment.consumables) \
             .filter(models.Consumable.expiration_date == expiration_date) \
             .options(
-                joinedload(models.Equipment.asset).joinedload(models.Asset.tenant),
-                joinedload(models.Equipment.family),
-                joinedload(models.Equipment.consumables).joinedload(models.Consumable.family),
+                joinedload(models.Asset.equipments).joinedload(models.Equipment.family),
+                joinedload(models.Consumable.family),
             ) \
+            .order_by(models.Tenant.tenant_id, models.Asset.asset_id) \
             .all()
 
-        for equipment in equipments:
-            tenant_id = equipment.asset.tenant_id
-            notifications.assets.consumables_expiration(request, tenant_id, equipment, expiration_date, delay_days)
+        if not assets:
+            continue
 
-        total_assets += len(equipments)
+        # Group assets by tenant.
+        groupby_asset = {}
+        for asset, consumable in assets:
+            if asset.asset_id not in groupby_asset:
+                groupby_asset[asset.asset_id] = {'asset': asset, 'consumables': [consumable]}
+            else:
+                groupby_asset[asset.asset_id]['consumables'].append(consumable)
+
+        total_assets += len(groupby_asset)
+
+        groupby_tenant = {}
+        for asset_dict in groupby_asset.values():
+            tenant_id = asset_dict['asset'].tenant.tenant_id
+            if tenant_id not in groupby_tenant:
+                groupby_tenant[tenant_id] = [asset_dict]
+            else:
+                groupby_tenant[tenant_id].append(asset_dict)
+
+        for tenant_id, assets in groupby_tenant.items():
+            notifications.assets.consumables_expiration(request, tenant_id, assets, expiration_date, delay_days)
 
     return total_assets
--- asset_tracker/notifications/assets.py
+++ asset_tracker/notifications/assets.py
@@ -33,35 +33,28 @@ def assets_calibration(request, tenant_id, assets, calibration_date):
     request.logger_technical.info(['notify calibration date', [asset.id for asset in assets]])
 
 
-def consumables_expiration(request, tenant_id, equipment, expiration_date, delay_days):
+def consumables_expiration(request, tenant_id, assets, expiration_date, delay_days):
     """Notify users with notifications-consumables right that the consumables of an equipment are expiring.
 
     Args:
         request (pyramid.request.Request).
         tenant_id (str).
-        equipment (asset_tracker.models.Equipment).
+        assets (list[asset_tracker.models.Asset]).
         expiration_date (date): consumable expiration date (YYYY-MM-DD).
         delay_days (int): number of days before expiration.
     """
-    expired_consumables = []
-    for consumable in equipment.consumables:
-        if consumable.expiration_date.strftime('%Y-%m-%d') == expiration_date:
-            expired_consumables.append(consumable.family.model)
-
     template_data = {
-        'asset': equipment.asset,
+        'assets': assets,
         'cloud_name': request.registry.settings['asset_tracker.cloud_name'],
         'delay_days': delay_days,
-        'expired_consumables': expired_consumables,
         'expiration_date': expiration_date,
     }
 
     # Email.
-    subject = _('Equipment consumables expiration reminder')
+    subject = _('Consumables expiration reminder')
     text = 'emails/consumables_expiration.txt'
     html = 'emails/consumables_expiration.html'
     emails = request.notifier.render_emails(subject, text, html, template_data)
-    request.logger_technical.info(emails)
 
     # Asynchronous POST.
     request.notifier.notify({
@@ -70,4 +63,4 @@ def consumables_expiration(request, tenant_id, equipment, expiration_date, delay
         'tenant': tenant_id,
     })
 
-    request.logger_technical.info(['notify consumables expiration', equipment.id])
+    request.logger_technical.info(['notify consumables expiration', [asset['asset'].id for asset in assets]])
