a30c85c7ba100a1f0210c5a6b212e2318122c96c
Olivier Le Moign <olemoign@parsys.com>
2021-12-31 16:20:45 +0100

    feat(deps): parsys utilities 4.1.3

--- setup.py
+++ setup.py
@@ -10,17 +10,17 @@ with open(here / 'CHANGES.md') as f:
     CHANGES = f.read()
 
 tracker = 'https://tracker.parsys.com/api/v4/projects/9/packages/pypi/files'
-parsys_utilities_4_1_2 = f'{tracker}/6e89ff6eccd287fba31079792db5bca75f86efe503581e8cbc7aaccb929569e0/parsys_utilities-4.1.2-py3-none-any.whl'  # noqa: E501
+parsys_utilities_4_1_3 = f'{tracker}/ae4f204e501446610aadf210445ced292e270e9942a8a88bd552ae53a3a2b714/parsys_utilities-4.1.3-py3-none-any.whl'  # noqa: E501
 requires = [
     'alembic==1.7.5',
     'arrow==1.2.1',
-    'celery[redis]==5.2.1',
+    'celery[redis]==5.2.3',
     'filedepot==0.8.0',
     'jinja2==3.0.3',
-    f'parsys-utilities @ {parsys_utilities_4_1_2}',
+    f'parsys-utilities @ {parsys_utilities_4_1_3}',
     'paste==3.5.0',
     'plaster-pastedeploy==0.7',
-    'psycopg2-binary==2.9.2',
+    'psycopg2-binary==2.9.3',
     'pyramid==1.10.8',
     'pyramid-assetviews==1.0a3',
     'pyramid-jinja2==2.8',
@@ -28,8 +28,8 @@ requires = [
     'pyramid-tm==2.4',
     'python-dateutil==2.8.2',
     'sentry-sdk==1.5.1',
-    'setuptools==60.0.0',
-    'sqlalchemy==1.4.28',
+    'setuptools==59.6.0',
+    'sqlalchemy==1.4.29',
     'transaction==3.0.1',
     'waitress==2.0.0',
     'webob==1.8.7',
