7b9e3156ff4dc0d7d94c8115f4c1e88885e1ff23
Olivier Le Moign <olemoign@parsys.com>
2022-02-15 11:21:28 +0100

    feat(scripts): update calibration_next

--- asset_tracker/scripts/2022-02-14_update_calibration_next.py
+++ asset_tracker/scripts/2022-02-14_update_calibration_next.py
@@ -19,15 +19,30 @@ def main():
     with bootstrap(args.config_uri, options=options) as env, env['request'].tm:
         db_session = env['request'].db_session
 
+        print('Consumable cases')
         for case in db_session.query(models.Asset).filter_by(asset_type='consumables_case'):
             case.calibration_next = None
             print(case.asset_id)
             print(case.calibration_next)
+        print()
 
+        print('Decommissioned')
+        assets = db_session.query(models.Asset) \
+            .join(models.Asset.status) \
+            .filter(models.EventStatus.status_id == 'decommissioned')
+        for asset in assets:
+            asset.calibration_next = None
+            print(asset.asset_id)
+            print(asset.calibration_next)
+        print()
+
+        print('Stations')
         delta = relativedelta(years=CALIBRATION_FREQUENCIES_YEARS['marlink'])
         assets = db_session.query(models.Asset) \
+            .join(models.Asset.status) \
             .filter(
                 models.Asset.asset_type != 'consumables_case',
+                models.EventStatus.status_id != 'decommissioned',
                 ~models.Asset.asset_id.ilike('STHT%'),
             )
         for asset in assets:
