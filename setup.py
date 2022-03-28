621e8baba780420aa54ff4f0c689d39a626228d4
Olivier Le Moign <olemoign@parsys.com>
2022-03-28 20:52:38 +0200

    feat(deps): parsys-utilities 4.2.5

--- setup.py
+++ setup.py
@@ -10,27 +10,27 @@ with open(here / 'CHANGES.md') as f:
     CHANGES = f.read()
 
 tracker = 'https://tracker.parsys.com/api/v4/projects/9/packages/pypi/files'
-parsys_utilities_4_2_4 = f'{tracker}/3ce39810019234fab2cf02e1a23a41dec729bea160928830c4fbf8167627fb14/parsys_utilities-4.2.4-py3-none-any.whl'  # noqa: E501
+parsys_utilities_4_2_5 = f'{tracker}/e80a244f065bffaef53e362449cfca28b25d964d5f6a013f42b44c9cf2a10f17/parsys_utilities-4.2.5-py3-none-any.whl'  # noqa: E501
 requires = [
-    'alembic==1.7.6',
+    'alembic==1.7.7',
     'arrow==1.2.2',
     'celery[redis]==5.2.3',
     'filedepot==0.8.0',
-    'jinja2==3.0.3',
-    f'parsys-utilities @ {parsys_utilities_4_2_4}',
+    'jinja2==3.1.1',
+    f'parsys-utilities @ {parsys_utilities_4_2_5}',
     'paste==3.5.0',
     'plaster-pastedeploy==0.7',
     'psycopg2==2.9.3',
     'pyramid==1.10.8',
     'pyramid-assetviews==1.0a3',
-    'pyramid-jinja2==2.8',
+    'pyramid-jinja2==2.9.2',
     'pyramid-session-redis==1.6.3',
-    'pyramid-tm==2.4',
+    'pyramid-tm==2.5',
     'python-dateutil==2.8.2',
-    'sentry-sdk==1.5.6',
+    'sentry-sdk==1.5.8',
     'sqlalchemy==1.4.32',
     'transaction==3.0.1',
-    'waitress==2.1.0',
+    'waitress==2.1.1',
     'webob==1.8.7',
     'zope.sqlalchemy==1.6',
 ]
@@ -45,7 +45,7 @@ optional = {
         'flake8==4.0.1',
     ],
     'tests': [
-        'pytest==7.0.1',
+        'pytest==7.1.1',
         'pytest-cov==3.0.0',
         'webtest==3.0.0',
     ],
