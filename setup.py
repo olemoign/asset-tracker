af88e649f3d1028cd76bf02c051a32bdbdc5c3f1
Olivier Le Moign <olemoign@parsys.com>
2022-06-15 20:29:11 +0200

    feat(deps): parsys-utilities 4.2.14

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ requires = [
     'filedepot==0.8.0',
     'jinja2==3.1.2',
     'packaging==21.3',
-    'parsys-utilities==4.2.12',
+    'parsys-utilities==4.2.14',
     'paste==3.5.0',
     'psycopg2==2.9.3',
     'pyramid==1.10.8',
@@ -34,7 +34,7 @@ requires = [
 
 optional = {
     'dev': [
-        'babel==2.10.1',
+        'babel==2.10.2',
         'pybabel-json-md==0.1.0',
         'pyramid-debugtoolbar==4.9',
     ],
